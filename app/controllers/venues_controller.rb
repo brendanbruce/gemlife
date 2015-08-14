a01307c334e4c92b8c6067e582cd2b966a181d0d
Brendan Bruce <brendanbruce@gmail.com>
2015-08-14 14:57:35 -0400

    Add functionality to create new venue

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,4 +6,39 @@ class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
   end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+
+    if @venue.save
+      redirect_to @venue, notice: "New Venue Added."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+
+    if @venue.update(venue_params)
+      redirect_to @venue, notice: "Venue information was updated"
+    else
+      render :edit
+    end
+
+  end
+
+  def venue_params
+    params.require(:venue).permit(:name, :description, :street_address, :city,
+                                  :state, :zip_code, :phone, :twitter_url,
+                                  :facebook_url, :website_url)
+  end
 end
