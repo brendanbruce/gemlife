c0e9f33bed7055f5887c4ca5226a420593818b02
Brendan Bruce <brendanbruce@gmail.com>
2016-04-07 18:19:49 -0400

    Create profile when new user is created

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
+    Profile.create(:user_id => resource.id)
     new_profile_path
   end
 end
