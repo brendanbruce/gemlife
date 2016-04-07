46f301a377e71d86a600ac251edce34f2dfe1cc4
Brendan Bruce <brendanbruce@gmail.com>
2016-04-07 17:52:17 -0400

    Add edit page for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,37 +1,47 @@
 class ProfilesController < ApplicationController
- def index
-   @profiles = Profile.all
- end
+  before_action :authenticate_user!, only: [:new]
+  before_action :require_permission, only: [:edit]
 
- def show
-   @profile = Profile.find(params[:id])
- end
+  def index
+    @profiles = Profile.all
+  end
 
- def new
-   @profile = Profile.new
- end
+  def show
+    @profile = Profile.find(params[:id])
+  end
 
- def edit
- end
+  def new
+    @profile = Profile.new
+  end
 
- def create
-   @profile = Profile.new(profile_params)
-   @profile.user_id = current_user.id
+  def edit
+    @profile = Profile.find(params[:id])
+  end
 
-   respond_to do |format|
-     if @profile.save
-       format.html { redirect_to @profile, notice: "Profile was successfully created." }
-       format.json { render :show, status: :created, location: @profile }
-     else
-       format.html { render :new }
-       format.json { render json: @profile.errors, status: :unprocessable_entity }
-     end
-   end
- end
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user_id = current_user.id
 
- private
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to @profile, notice: "Profile was successfully created." }
+        format.json { render :show, status: :created, location: @profile }
+      else
+        format.html { render :new }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
 
   def profile_params
     params.require(:profile).permit(:name, :bio, :website, :twitter, :facebook, :soundcloud, :mixcloud, :dj)
+  end
+
+  def require_permission
+    if current_user.profile != Profile.find(params[:id])
+      redirect_to action: "show"
+    end
   end
 end
