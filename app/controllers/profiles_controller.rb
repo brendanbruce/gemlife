fd937975399799d1f21c6773840d1ffecfd29276
Brendan Bruce <brendanbruce@gmail.com>
2016-04-07 15:03:19 -0400

    Add basic layouts for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,7 @@ class ProfilesController < ApplicationController
  end
 
  def show
+   @profile = Profile.find(params[:id])
  end
 
  def new
@@ -11,6 +12,26 @@ class ProfilesController < ApplicationController
  end
 
  def edit
-
  end
+
+ def create
+   @profile = Profile.new(profile_params)
+   @profile.user_id = current_user.id
+
+   respond_to do |format|
+     if @profile.save
+       format.html { redirect_to @profile, notice: "Profile was successfully created." }
+       format.json { render :show, status: :created, location: @profile }
+     else
+       format.html { render :new }
+       format.json { render json: @profile.errors, status: :unprocessable_entity }
+     end
+   end
+ end
+
+ private
+
+  def profile_params
+    params.require(:profile).permit(:name, :bio, :website, :twitter, :facebook, :soundcloud, :mixcloud, :dj)
+  end
 end
