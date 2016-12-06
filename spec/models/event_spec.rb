e3ff5f3e449df92c870f618f1a1d858148217a7c
Brendan Bruce <brendanbruce@gmail.com>
2016-12-06 18:15:02 -0500

    Add model validations for event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:start_date) }
+  it { should validate_presence_of(:start_time) }
+
+  it { should belong_to(:venue) }
+  it { should accept_nested_attributes_for(:venue) }
+
+  it { should have_many(:gigs) }
+  it { should have_many(:profiles).through(:gigs) }
+  it { should accept_nested_attributes_for(:gigs) }
+  it { should accept_nested_attributes_for(:profiles) }
+
+  describe ".likes_count" do
+    it "should return the number of likes on an event" do
+   
+    end
+  end
 end
