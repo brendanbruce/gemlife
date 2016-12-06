7786be5802fb3e3d8651bcd18601152130f10b56
Brendan Bruce <brendanbruce@gmail.com>
2016-12-06 18:16:04 -0500

    Add view tests for event show page

--- spec/views/events/show.html.haml_spec.rb
+++ spec/views/events/show.html.haml_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "events/show", type: :view do
   before(:each) do
+    @profile = assign(:profile, Profile.create!(
+      :name => "DJ Brendan"
+    ))
+
+    @venue = assign(:venue, Venue.create!(
+      :name => "Trophy"
+    ))
+
     @event = assign(:event, Event.create!(
-      :description => "MyText",
-      :event_link => "Event Link"
+      :title => "MyTitle",
+      :description => "EventDescription",
+      :start_date => Date.today,
+      :start_time => Time.now,
+      :venue_id => @venue.id,
+      :profile => @profile.name
     ))
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes" do
     render
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/Event Link/)
+
+    expect(rendered).to match(/MyTitle/)
+    expect(rendered).to match(/EventDescription/)
+    expect(rendered).to match(/DJ Brendan/)
   end
 end
