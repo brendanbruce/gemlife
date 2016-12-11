b16ab7606ea6c1dd4c19c727f5190c9288e5c805
Brendan Bruce <brendanbruce@gmail.com>
2016-12-11 12:20:14 -0500

    Add tests for Venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,5 @@
 class Venue < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   has_many :events
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Venue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "factory" do
+    it "has a valid factory" do
+      create(:venue).should be_valid
+    end
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "associations" do
+    it { should have_many(:events) }
+  end
 end
