46603518c3cdfb02b0548c34743eca09b4e3327b
Brendan Bruce <brendanbruce@gmail.com>
2016-12-11 12:20:27 -0500

    Add tests for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_uniqueness_of(:user_id).scoped_to(:event_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:event) }
+    it { should belong_to(:user) }
+  end
 end
