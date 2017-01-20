31939c1a27667dfb563d77e6423478c27b4360d0
Brendan Bruce <brendanbruce@gmail.com>
2017-01-20 18:10:52 -0500

    Update factories to create event scheduled today

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
   factory :event do |f|
     f.title { Faker::Book.title }
     f.description { Faker::Hipster.sentence }
-    f.start_date Date.tomorrow
-    f.start_time Time.now + (2*7*24*60*60)
+    f.start_date Date.today
+    f.start_time Time.now
     user
     venue
   end
