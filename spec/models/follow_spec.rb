4ed3b0a79fdf985c0d4aa965ca286f532fcb2fd9
Brendan Bruce <brendanbruce@gmail.com>
2017-01-30 18:37:40 -0500

    Add spec for follows

---
+++ spec/models/follow_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe Follow, type: :model do
+  describe "factory" do
+    it "has a valid factory" do
+      create(:follow).should be_valid
+    end
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:profile_id) }
+    it { should validate_presence_of(:followable_id) }
+    it { should validate_presence_of(:followable_type) }
+  end
+
+  describe "assocations" do
+    it { should belong_to(:profile) }
+  end
+end
