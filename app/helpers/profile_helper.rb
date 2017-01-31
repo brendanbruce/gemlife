606f276d74f3f514133f1c986fe7aba416efd561
Brendan Bruce <brendanbruce@gmail.com>
2017-01-30 21:57:39 -0500

    Style profile tab links

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -2,4 +2,12 @@ module ProfileHelper
   def member_since_date(time)
     time.strftime("%B %Y")
   end
+
+  def active_action(action)
+    if action == controller.action_name
+      "active"
+    else
+      "link"
+    end
+  end
 end
